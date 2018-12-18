6c1f56404997d659a8afa42f66e77078c95c1efc
Andreas Bissenbach Petersen <andr280b@edu.sde.dk>
2018-12-18 16:44:20 +0100

    commit

---
+++ Chatten/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chatten
+{
+    public partial class Form1 : Form
+    {
+        //Objekter
+        LinqMailDataContext db = new LinqMailDataContext();
+        Controller c = new Controller();
+        LoginResult r = new LoginResult();
+        AppDomain SecondaryDomain;
+        public string[] args;
+        public string mail;
+        Form opener;
+        Form2 f2 = new Form2();
+        string userName = "";
+        public Form1(Form ParentForm, string k)
+        {
+            
+            InitializeComponent();
+            userName = k;
+            opener = ParentForm;
+            SecondaryDomain = AppDomain.CreateDomain("Mail.exe");
+            var otherType = typeof(OtherProgram);
+            var obj = SecondaryDomain.CreateInstanceAndUnwrap(
+                                     otherType.Assembly.FullName,
+                                     otherType.FullName) as OtherProgram;
+
+            foreach (var n in from x in db.Logins where x.Name == k select x.Mail)
+            args = new[] { "mail is: " + n };
+
+            obj.Main(args, label2);
+
+
+            ActiveControl = textBox1;
+           label1.Text = $"Main domain is: {AppDomain.CurrentDomain.FriendlyName}";
+        }
+        private async void TextBox1_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                if (textBox1.Text == "swipe /all")
+                {
+                    await Task.Run(() => c.EntityDelete());
+                    dataGridView1.DataSource = "";
+                }
+                else
+                {
+                    mail = "abpbissen@gmail.com";
+                    await Task.Run(() => c.EntityctTbl(textBox1.Text, userName, mail));
+                    textBox1.Text = "";
+                }
+            }
+        }
+        private async void timer1_Tick_1(object sender, EventArgs e)
+        {
+            await Task.Run(() => ParentForm);
+            dataGridView1.DataSource = from x in db.ctTbls orderby x.ChatNr descending select new { x.ChatNr, x.Navn, x.Besked };
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            //Lukker Form2, når form1 lukkes
+            Application.Exit();
+        }
+    }
+
+}
---
+++ Chatten/Form2.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chatten
+{
+    
+    public partial class Form2 : Form
+    {
+        //Objekter
+        LinqMailDataContext db = new LinqMailDataContext();
+        Controller c = new Controller();
+        LoginResult r = new LoginResult();
+
+        public Form2()
+        {
+            InitializeComponent();
+            textBox4.UseSystemPasswordChar = true;
+            textBox2.UseSystemPasswordChar = true;
+            Visibility(false);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            r = c.LoginGruppe(textBox1.Text, textBox2.Text);
+            if (r.Mode)
+            {
+                Form1 f1 = new Form1(this, textBox1.Text);
+                textBox1.Text = r.LoginUser;
+                f1.Show();
+                Hide();
+               
+            }
+            else
+            {
+                label1.Text = "error";
+            }
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Visibility(true);
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            c.EntityLogin(textBox5.Text, textBox4.Text, textBox3.Text);
+            label1.Text = "Bruger oprettet";
+            Visibility(false);
+            
+        }
+
+        public bool Visibility(bool b)
+        {
+            if(b)
+            {
+                textBox5.Visible = true;
+                textBox4.Visible = true;
+                textBox3.Visible = true;
+                button2.Visible = true;
+                label2.Visible = true;
+            }
+            else
+            {
+                textBox5.Visible = false;
+                textBox4.Visible = false;
+                textBox3.Visible = false;
+                button2.Visible = false;
+                label2.Visible = false;
+            }
+            return b;
+        }
+
+    }
+    public class OtherProgram : MarshalByRefObject
+    {
+        public void Main(string[] args, Label L)
+        {
+
+            foreach (var item in args)
+                L.Text = $" {item} and is executed from this domain: {AppDomain.CurrentDomain.FriendlyName}";
+        }
+    }
+}
